720650db91b6bdfa787ca1a1dc3627d36902c653
MuhammedEsmail <abohadida51@gmail.com>
2025-02-12 19:08:45 +0200

    Add files via upload

--- src/DB/models/users.model.js
+++ src/DB/models/users.model.js
@@ -104,8 +104,8 @@ const userSchema = new mongoose.Schema({
     }],
     tempEmail: {
         type: String,
-        unique: true,
-        lowercase: true
+        lowercase: true,
+        match: /^(?!.*\.{2})[a-zA-Z0-9][a-zA-Z0-9#$%&\*\+-/=\?\_`|~]*@[a-zA-Z0-9][a-zA-Z0-9-_.]*\.[a-zA-Z]{2,4}$/
     },
     otpNewEmail: String,
     changeEmailOtp: String,
